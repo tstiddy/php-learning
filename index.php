bde7252bef4c63d7e507cf477a3fd4f2e1787a0c
tstiddy <spaceinvadini99@gmail.com>
2020-02-16 20:54:54 +0000

    php registration

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
         <?php
             echo "<p>Hello World!<p>";
         ?>
+        <a href="login.php"> Click here to login </a>
+        <br/>
+        <a href="register.php"> Click here to register </a>
     </body>
 </html>
 
---
+++ register.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>My first PHP Website</title>
+    <head>
+    <body>
+        <h2>Registration Page</h2>
+        <a href="index.php">Click here to go back </a><br/><br/>
+        <form action="register.php" method="POST">
+            Enter Username: <input type="text" name="username" required="required" />
+            <br/>
+            Enter Password: <input type="password" name="password" required="required" />
+            <br />
+            <input type="submit" value="Register"/>
+        </form>
+    </body>
+</html>
