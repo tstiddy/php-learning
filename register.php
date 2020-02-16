d53032272a1c8cd89e1cdcd33b520bf0025822c1
tstiddy <spaceinvadini99@gmail.com>
2020-02-16 21:26:36 +0000

    php login

--- register.php
+++ register.php
@@ -6,11 +6,16 @@
         <h2>Registration Page</h2>
         <a href="index.php">Click here to go back </a><br/><br/>
         <form action="register.php" method="POST">
-            Enter Username: <input type="text" name="username" required="required" />
-            <br/>
-            Enter Password: <input type="password" name="password" required="required" />
-            <br />
+            Enter Username: <input type="text" name="username" required="required"/> <br/>
+            Enter Password: <input type="password" name="password" required="required"/> <br />
             <input type="submit" value="Register"/>
         </form>
     </body>
 </html>
+
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = mysql_real_escape_string($_POST['username']);
+        $password = mysql_real_escape_string($_POST['password']);
+    }
+?>
